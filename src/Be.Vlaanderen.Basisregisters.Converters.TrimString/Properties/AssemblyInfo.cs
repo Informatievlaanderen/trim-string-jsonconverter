aabfb8878ad60d1cbd0ae1d9f9b847962a01330a
David Cumps <david@cumps.be>
2018-12-15 23:36:30 +0100

    feat: open source with MIT license as 'agentschap Informatie Vlaanderen'

---
+++ src/Be.Vlaanderen.Basisregisters.Converters.TrimString/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Vlaamse basisregisters")]
+[assembly: AssemblyDescription("JSON.NET converter for trimming and removing duplicate spaces in strings.")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("a2add1f3-b279-48c8-9049-0b257375fe0c")]
+
+[assembly: NeutralResourcesLanguage("en")]
